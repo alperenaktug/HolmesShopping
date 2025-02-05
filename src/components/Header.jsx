d2005c298ab575d0267f9260869581d88aaa6ec5
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-02-05 21:55:03 +0300

    header update2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,17 @@
-import { useState } from "react";
 import "../css/Header.css";
 import { GrBasket } from "react-icons/gr";
+import { BsLightningChargeFill } from "react-icons/bs";
+
 import { BsLightningCharge } from "react-icons/bs";
-import { Icon } from "@mui/material";
 
 function Header() {
-  // Tema durumunu tutmak için state oluşturuyoruz
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-  // Tema değiştirme fonksiyonu
-  const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-  };
-
-  // Temaya göre arka plan ve metin rengini belirliyoruz
-  const themeStyles = {
-    backgroundColor: isDarkTheme ? "#333" : "#fff",
-    color: isDarkTheme ? "#fff" : "#333",
-  };
-
   return (
     <div
-      className="header"
       style={{
-        ...themeStyles,
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        padding: "10px 20px",
       }}
     >
       <div className="flex-row">
@@ -36,22 +19,10 @@ function Header() {
         <p className="logo-text">Holmes Shopping</p>
       </div>
       <div className="flex-row">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Searching"
-          style={{ backgroundColor: isDarkTheme ? "default" : "#f0f0f0" }}
-        />
+        <input className="search-input" type="text" placeholder="Searching" />
         <div className="flex-row">
-          {/* Tema değiştirme butonu */}
-          <Icon onClick={toggleTheme}>
-            {isDarkTheme ? (
-              <BsLightningCharge />
-            ) : (
-              <BsLightningCharge className="icon" />
-            )}
-          </Icon>
-
+          <BsLightningChargeFill className="icon" />
+          <BsLightningCharge className="icon" />
           <GrBasket className="icon" />
         </div>
       </div>
