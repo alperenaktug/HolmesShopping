757cad20e5d37ca54bfe6e370cb9bd43f7636ce5
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-02-05 22:49:29 +0300

    redux initial

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,14 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
+
+createRoot(document.getElementById("root")).render;
+
+{
+  <Provider store={store}>
+    (<App />
+    );
+  </Provider>;
+}
