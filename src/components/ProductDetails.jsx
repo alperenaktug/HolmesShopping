fa4e9ab3d575d2db88fde99f816b9eb20e0b0f33
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-02-09 16:33:12 +0300

    buton , increment decrement use

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,13 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { setSelectedProduct } from "../redux/slices/productSlice";
+import { FiPlusSquare } from "react-icons/fi";
+import { FiMinusSquare } from "react-icons/fi";
 
 function ProductDetails() {
   const { id } = useParams();
   const { products, selectedProduct } = useSelector((store) => store.product);
   const { price, image, title, description } = selectedProduct;
+  const [count, setCount] = useState(0); // useState doğru şekilde kullanıldı
   const dispatch = useDispatch();
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
 
   useEffect(() => {
     getProductById();
@@ -42,12 +55,43 @@ function ProductDetails() {
             fontSize: "40px",
             fontFamily: "arial",
             fontWeight: "bold",
-            color: "red",
+            color: "hotpink",
           }}
         >
           {" "}
           {price}₺{" "}
         </h1>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <FiPlusSquare
+            onClick={increment}
+            style={{ fontSize: "40px", marginRight: "5px" }}
+          />
+          <span
+            style={{
+              fontSize: "35px",
+            }}
+          >
+            {count}
+          </span>
+          <FiMinusSquare
+            onClick={decrement}
+            style={{ fontSize: "40px", marginLeft: "5px" }}
+          />
+        </div>
+        <div>
+          <button
+            style={{
+              marginTop: "20px",
+              padding: "12px",
+              borderRadius: "5px",
+              border: "none",
+              cursor: "pointer",
+              backgroundColor: "hotpink",
+            }}
+          >
+            add to cart
+          </button>
+        </div>
       </div>
     </div>
   );
